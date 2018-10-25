022df0002963765254b6c5f34d61f312383b2feb
Medi Assumani <mediassumani49@gmail.com>
2018-10-25 10:26:08 -0700

    Complete dictionary_words.py

--- dictionary_words.py
+++ dictionary_words.py
@@ -4,13 +4,13 @@ import linecache
 
 def main():
 
-    print(generateSentence(6))
+    print(generateSentence(int(sys.argv[1])).replace("\n", " "))
 
 def generateSentence(num_of_words):
     count = 0
     sentence = ""
     try:
-        while count <= num_of_words:
+        while count < num_of_words:
             sentence += linecache.getline("/usr/share/dict/words", random.randint(0, 235885))
             count += 1
 
