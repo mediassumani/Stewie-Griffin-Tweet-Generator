2856db531e21ffa878078b4fcb47e6d1280feae0
Medi Assumani <mediassumani49@gmail.com>
2018-10-25 11:31:49 -0700

    Add timing decorator

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,24 +1,30 @@
 import sys
-import random
 import linecache
+from random import randint
+from algorithm_timer import timing_function
 
-def main():
 
-    print(generateSentence(int(sys.argv[1])).replace("\n", " "))
-
-def generateSentence(num_of_words):
+def generateSentence(num_words):
+    """ Grabs n random words from the text file"""
     count = 0
     sentence = ""
     try:
-        while count < num_of_words:
-            sentence += linecache.getline("/usr/share/dict/words", random.randint(0, 235885))
+        while count < num_words:
+            sentence += linecache.getline("/usr/share/dict/words", randint(0, 235885))
             count += 1
-
     except IOError:
         print("Error Found : Unable to find file")
 
     return sentence
 
+@timing_function
+def tester():
+    print(generateSentence(int(sys.argv[1])).replace("\n", " "))
+print(tester())
+
+def main():
+
+    tester()
 
 if __name__ == "__main__":
     main()
