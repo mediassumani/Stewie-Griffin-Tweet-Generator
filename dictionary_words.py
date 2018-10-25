9145a5371dcc9458be3b6931961b893cecb639ae
Medi Assumani <mediassumani49@gmail.com>
2018-10-24 23:03:58 -0700

    Done with dictionary_words.py

--- dictionary_words.py
+++ dictionary_words.py
@@ -9,15 +9,15 @@ def main():
 def generateSentence(num_of_words):
     count = 0
     sentence = ""
-    rand_index = random.randint(0, 235885)
     try:
         while count <= num_of_words:
-            sentence += linecache.getline("/usr/share/dict/words", rand_index)
+            sentence += linecache.getline("/usr/share/dict/words", random.randint(0, 235885))
             count += 1
 
-    return sentence
     except IOError:
         print("Error Found : Unable to find file")
+
+    return sentence
 
 
 if __name__ == "__main__":
