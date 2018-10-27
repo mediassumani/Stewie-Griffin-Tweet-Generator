2a59e11c02c3e61bd1f228bd66f06298fd978dc3
Medi Assumani <mediassumani49@gmail.com>
2018-10-27 15:11:02 -0700

    Working on histogram_dict.py

---
+++ histogram_dict.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+
+
+def histogram(source_text):
+    """ return a histogram data structure that stores each unique word along with the number of times the word appears in the source text."""
+    return
+
+def unique_words(histogram):
+    """ returns the total count of unique words in the histogram"""
+
+    return
+
+def frequency():
+    return
+
+def main():
+
+
+
+
+
+
+if __name__ == "__main__":
