1fdd6b20ad29f908349ecea0e358822ca1ebc268
Medi Assumani <mediassumani49@gmail.com>
2018-10-27 15:54:56 -0700

    making progress

--- histogram_dict.py
+++ histogram_dict.py
@@ -4,22 +4,40 @@ import sys
 
 
 def histogram(source_text):
-    """ return a histogram data structure that stores each unique word along with the number of times the word appears in the source text."""
-    return
+    """ returns a histogram that stores each unique word with the number of times it appears in the source text"""
+
+    split_sentence = []
+    histogram = {}
+    try:
+        with open(source_text, "r") as file:
+            for line in file:
+                split_sentence += line.split()
+                for word in split_sentence:
+                    histogram[word] = 1
+
+
+        file.close()
+        return histogram
+    except IOError:
+        print("Error Found while opening the file")
+
+
+
 
 def unique_words(histogram):
     """ returns the total count of unique words in the histogram"""
 
     return
 
-def frequency():
+def frequency(word, histogram):
+    """ returns the number of times that word appears in a text"""
     return
 
 def main():
 
-
-
-
+    text_file = "source_text.txt"
+    print(histogram(text_file))
 
 
 if __name__ == "__main__":
+    main()
