8b0d7f425cc23e2cc29244a950cd29170fda0b11
Medi Assumani <mediassumani49@gmail.com>
2018-10-27 22:07:58 -0700

    working on histogram_tuple.py

---
+++ SectionThree/histogram_tuple.py
@@ -0,0 +1,30 @@
+""" Modules contains functions to process an histogram of tuple"""
+from histogram_dict import frequency
+
+def histogram(source_text):
+    split_sentence = []
+    histogram = [()]
+    try:
+        with open(source_text) as file:
+            for line in file:
+                split_sentence += line.split()
+                for word in split_sentence:
+                    histogram.append((word, frequency(word, split_sentence)))
+
+    except IOError:
+        print("Errror Found while opening file")
+
+    return histogram
+
+
+def unique_words(histogram):
+    return
+
+
+
+def main():
+    text_file = "source_text.txt"
+    print(histogram(text_file))
+
+if __name__ == "__main__":
+    main()
