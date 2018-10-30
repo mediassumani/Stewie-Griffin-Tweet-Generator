4aee77d05409382f0c8eb5024d5582f3d74d1198
Medi Assumani <mediassumani49@gmail.com>
2018-10-29 22:40:15 -0700

    benchmarking algorithm

--- SectionThree/histogram_dict.py
+++ SectionThree/histogram_dict.py
@@ -1,5 +1,9 @@
 """ Modules contains functions to process an histogram of dictionary"""
-
+config_file = '/Users/mediassumani/Documents/dev/cs/Courses/CS1.2/algorithm_timer.py'
+import sys
+import os
+sys.path.append(os.path.dirname(config_file))
+from algorithm_timer import timing_function
 
 def histogram(source_text):
     """ returns a histogram that stores each unique word with the number of times it appears in the source text"""
@@ -9,17 +13,12 @@ def histogram(source_text):
     try:
         with open(source_text, "r") as file:
             for line in file:
-                if line == "":
-                    pass
-                else:
-                    split_sentence += line.split()
-                    for word in split_sentence:
-                        dict_histogram[word] = frequency(word, split_sentence)
+                print(line.split())
 
         file.close()
     except IOError:
         print("Error Found while opening the file")
-    print(unique_words(dict_histogram))
+    print("There are {} unique words in this histogram".format(unique_words(dict_histogram)))
     return dict_histogram
 
 def unique_words(histogram):
@@ -36,15 +35,18 @@ def frequency(word, text):
     """ returns the number of times that word appears in a text"""
     frequency_count = 0
     for str in text:
-        if word == str.lower() or word.upper() == str.upper():
+        if word == str.lower() or word == str.upper() or word == str+".":
             frequency_count += 1
 
     return frequency_count
 
-def main():
+
+@timing_function
+def tester():
     text_file = "source_text.txt"
-    histogram(text_file)
+    print(histogram(text_file))
+print(tester())
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__tester__":
+    tester()
