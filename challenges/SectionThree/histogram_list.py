5425f40d7f78a6dc9763ea8b5ed34157063154cf
Medi Assumani <mediassumani49@gmail.com>
2018-11-07 15:29:51 -0800

    Add comments and refactor code

--- challenges/SectionThree/histogram_list.py
+++ challenges/SectionThree/histogram_list.py
@@ -21,26 +21,30 @@ def histogram(source_text):
         if not found_word:
             # gives a frequency of 1 if word never seen before
             list_histogram.append([word, 1])
-            
+
     return list_histogram
 
 def frequency(word, histogram):
     """ returns the number of times that word appears in a text"""
-    for array in histogram:
+    for list in histogram:
+        # if word is seen, returns its frequency
         if array[0] == word:
             return array[1]
+
+    return 0 # returns zero if the word is not in histogram
 
 def unique_words(tuple_histogram):
     """ returns the total count of unique words in the histogram"""
     return len(tuple_histogram)
 
 def main():
+
+    # Testing the methods created above
     with open("source_text.txt") as text_file:
         words = text_file.read().replace("\n", "").lower().split()
     created_histogram = histogram(words)
     print("\n\n\t\t **** HISTOGRAM DATA *** \n\n{}\n\nThere are {} unique words".format(created_histogram, unique_words(created_histogram)))
     print("The word one appears {} times in the histogram".format(frequency("one",created_histogram)))
-
 
 
 if __name__ == "__main__":
