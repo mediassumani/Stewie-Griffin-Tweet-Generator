cbbccf8a2e955156c6ceac51e293a8fb0ed0d214
Medi Assumani <mediassumani49@gmail.com>
2018-11-15 10:16:29 -0800

    changes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ DESCRIPTION:            Entry point of the Stswie Griffin Tweet Generator that c
 
 from flask import Flask
 import sys
+import random
 sys.path.insert(0,'./scripts')
 from file_opener import read_file
 from histogram_generator import generate_histogram
@@ -24,7 +25,7 @@ def index():
     # return generate_sentence(histogram, 10)
     return "I am amazing!"
 
-dict_of_dict = {}
+markov_model = {}
 
 text_list = read_file("fish.txt")
 histogram = generate_histogram(text_list)
@@ -32,7 +33,7 @@ histogram = generate_histogram(text_list)
 
 for key,value in histogram.items():
     temp_value = []
-    if key not in dict_of_dict:
+    if key not in markov_model:
         temp_key = key
 
         # for index, word in enumerate(text_list):
@@ -44,5 +45,8 @@ for key,value in histogram.items():
             #     temp_value.append(text_list[i+1])
                 # dict_of_dict[key] += temp_value
 
-    dict_of_dict[key] = temp_value
-print(dict_of_dict)
+    markov_model[key] = temp_value
+
+for key, value in markov_model.items():
+    random_index = random.randint(0, len(value) - 1)
+    print(key, value[random_index])
