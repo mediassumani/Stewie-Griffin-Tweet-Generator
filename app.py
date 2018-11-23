122725b4439f94d7468a0989617a1733333e0791
Medi Assumani <mediassumani49@gmail.com>
2018-11-22 20:02:41 -0800

    [Add] perfom random walk on markov model

--- app.py
+++ app.py
@@ -25,27 +25,26 @@ def index():
     return "I am amazing!"
 
 markov_model = {}
-
-text_list = read_file("fish.txt")
+text_list = read_file("dummy.txt")
 histogram = generate_histogram(text_list)
+sentence = ""
 
 
 for key,value in histogram.items():
     temp_value = []
     if key not in markov_model:
         temp_key = key
-
-        # for index, word in enumerate(text_list):
         for i in range(0, len(text_list)-1):
             if text_list[i] == temp_key:
                 temp_value.append(text_list[i + 1])
 
-            # if text_list[i+1] is not None:
-            #     temp_value.append(text_list[i+1])
-                # dict_of_dict[key] += temp_value
-
     markov_model[key] = temp_value
-
 for key, value in markov_model.items():
-    random_index = random.randint(0, len(value) - 1)
-    print(key, value[random_index])
+    if len(value) == 0:
+        break
+    elif len(value) != 0:
+        random_index = random.randint(0, len(value)-1)
+        sentence += value[random_index]
+    # there's an empty list, figure it out!!!
+
+print(sentence)
