e2d69c31e9433f43ecaa651a62a817b681475940
Medi Assumani <mediassumani49@gmail.com>
2018-12-07 13:46:26 -0800

    [fix] first order markov chain

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from random import randint
 from file_opener import read_file
 from histogram_generator import generate_histogram
 from sentence import generate_sentence
+from stochatic_sample import dict_frequency_sample
 
 app = Flask(__name__)
 
@@ -29,45 +30,49 @@ text_list = read_file("dummy.txt")
 histogram = generate_histogram(text_list)
 sentence = ""
 
-
-for key,value in histogram.items():
-    temp_value = []
+for key, value in histogram.items():
+    temp_value = {}
     if key not in markov_model:
         temp_key = key
         for i in range(0, len(text_list)-1):
-            if text_list[i] == temp_key:
-                temp_value.append(text_list[i + 1])
-
+            temp_word = text_list[i]
+            if temp_word == temp_key:
+                next_word = text_list[i + 1]
+                temp_value[next_word] = histogram.frequency(next_word)
     markov_model[key] = temp_value
 
-
 for key,value in markov_model.items():
+    current_state = key
     if len(sentence) == 0:
-        sentence = "{} ".format(key)
+        sentence = "{}".format(current_state)
+    # selected_word = dict_frequency_sample(value)
+    # sentence += selected_word
 
-    elif len(value) == 0:
+    if len(value) == 0:
         sentence += key
         break
+    for innerKey,innerValue in markov_model.items():
+        if innerKey == current_state:
+            nested_word = dict_frequency_sample(innerValue)
+            sentence += " {} ".format(nested_word)
 
-    else:
-        random_index = randint(0, len(value)-1)
-        random_choice = value[random_index]
-        sentence += random_choice
-        for innerKey,innerValue in markov_model.items():
-            if innerKey == random_choice and innerKey != "STOP":
-                rand_index = randint(0, len(innerValue)-1)
-                sentence += " {} ".format(innerValue[rand_index])
-                break
+        elif len(innerValue) == 0:
+            pass
+            #sentence += innerKey
+        current_state = nested_word
+
+    # else:
+        # selected_word = dict_frequency_sample(value)
+        # sentence += selected_word
+        # print(key,value)
+        # for innerKey,innerValue in markov_model.items():
+        #     if len(innerValue) == 0:
+        #         break
+        #     if innerKey == selected_word:
+        #         sentence += " {} ".format(dict_frequency_sample(innerValue))
+        #         break
 
 
-# for key, value in markov_model.items():
-#     if len(value) == 0:
-#         break
-#     elif "STOP" in value:
-#         break
-#     else:
-#         random_index = random.randint(0, len(value)-1)
-#         sentence += " {}".format(value[random_index])
-    # there's an empty list, figure it out!!!
+
 print(markov_model)
-print("\n\n Sentence generated : {} \n\n".format(sentence))
+print("\n\nsentence generated : {} \n".format(sentence))
