dc16c00c8dccb44d67377836ee8767f9fb55d7af
Medi Assumani <mediassumani49@gmail.com>
2018-12-08 15:18:27 -0800

    [Add] generate histogram function

--- dictogram.py
+++ dictogram.py
@@ -48,25 +48,3 @@ def print_histogram(word_list):
         freq = histogram.frequency(word)
         print('{!r} occurs {} times'.format(word, freq))
     print()
-
-
-def main():
-    arguments = sys.argv[1:]  # Exclude script name in first argument
-    if len(arguments) >= 1:
-        # Test histogram on given arguments
-        print_histogram(arguments)
-    else:
-        # Test histogram on letters in a word
-        word = 'abracadabra'
-        print_histogram(list(word))
-        # Test histogram on words in a classic book title
-        fish_text = 'one fish two fish red fish blue fish'
-        print_histogram(fish_text.split())
-        # Test histogram on words in a long repetitive sentence
-        woodchuck_text = ('how much wood would a wood chuck chuck'
-                          ' if a wood chuck could chuck wood')
-        print_histogram(woodchuck_text.split())
-
-
-if __name__ == '__main__':
-    main()
--- second_order_markov.py
+++ second_order_markov.py
@@ -12,10 +12,15 @@ class Second_Markov(dict):
         self.sentence = ""
 
     def _create_histogram(self):
-        pass
+        return generate_histogram(self.words)
 
     def _create_model(self):
         pass
 
     def generate_sentence(self):
         pass
+
+
+text_list = read_file("stewi_griffin_scripts.txt")
+markov_model = Second_Markov(text_list)
+print(markov_model._create_histogram())
