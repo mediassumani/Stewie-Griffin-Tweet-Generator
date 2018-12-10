c31f15ec95a2f3d7cb033b278480c333d6134da2
Medi Assumani <mediassumani49@gmail.com>
2018-12-09 21:04:47 -0800

    [Add] style

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ DESCRIPTION:            Entry point of the Stewie Griffin Tweet Generator that c
 """
 
 from flask import Flask
-import sys
-from random import randint
 from file_opener import read_file
-from histogram_generator import generate_histogram
-from sentence import generate_sentence
 from stochatic_sample import dict_frequency_sample
 from markov import Markov
 
@@ -21,4 +17,5 @@ def index():
 
     text_list = read_file("stewi_griffin_scripts.txt")
     markov_model = Markov(text_list)
+    print(len(text_list))
     return  markov_model.random_walk(30)
