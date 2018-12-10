419eaa4f61339b3ad8cd654f7e63a72fce99323f
Medi Assumani <mediassumani49@gmail.com>
2018-12-09 21:29:20 -0800

    organizing files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ DESCRIPTION:            Entry point of the Stewie Griffin Tweet Generator that c
                         the flask server and connected with needed internal modules.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 from file_opener import read_file
 from stochatic_sample import dict_frequency_sample
 from markov import Markov
@@ -17,5 +17,4 @@ def index():
 
     text_list = read_file("stewi_griffin_scripts.txt")
     markov_model = Markov(text_list)
-    print(len(text_list))
-    return  markov_model.random_walk(30)
+    return  render_template("home.html", sentence= markov_model.random_walk(30))
